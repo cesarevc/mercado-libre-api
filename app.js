491507780e8b0bc0e7fc55eaa63120444a84d1b6
Vazquez Cesar <cesar.evc@outlook.com>
2020-07-08 12:51:53 -0500

    ML API

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-// const { getCompanyData, getArticles } = require('./getData');
-
 const routes = require('./routes')
 
 // middleware
